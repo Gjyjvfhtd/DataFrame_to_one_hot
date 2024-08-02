726458795e129e26ac251af0b82142c04f4a57e0
Alexey Ponomarev <alexeyponomarev@Noutbuk-Alexey.local>
2024-08-02 18:03:53 +0500

    add comments

--- data_frame_hot_one.py
+++ data_frame_hot_one.py
@@ -1,9 +1,25 @@
-import random
-import pandas as pd
+#Ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. 
 
-lst = ['robot'] * 10
-lst += ['human'] * 10
-random.shuffle(lst)
-data = pd.DataFrame({'whoAmI': lst})
+#lst = ['robot'] * 10
+#lst += ['human'] * 10
+#random.shuffle(lst)
+#data = pd.DataFrame({'whoAmI': lst})
+#return data
 
+#Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
+
+
+
+
+import random           #Библиотека для рандомизации значений, тут нужна для создания DataFrame
+import pandas as pd     #Библиотека для работы с DataFrame
+
+def random_data_frame(): #Спрятал генератор DataFrame в функцию, чтобы не мешал)
+    lst = ['robot'] * 10
+    lst += ['human'] * 10
+    random.shuffle(lst)
+    data = pd.DataFrame({'whoAmI': lst})
+    return data
+
+data = random_data_frame()  #В этом блоке я загоняю все в csv файл - дальше работа будет вестись с ним
 data.to_csv('who_i_am.csv')
